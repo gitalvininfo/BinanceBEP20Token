40fea4f9382574c0776e83bc4c54fadccdc8a9cd
Alvin <yanson.alvin.info@gmail.com>
2021-09-29 17:45:48 +0800

    contract ownable implementation

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+// scripts/index.js
+module.exports = async function main(callback) {
+    try {
+        // Our code will go here
+
+        // Retrieve accounts from the local node
+        const accounts = await web3.eth.getAccounts();
+        console.log(accounts)
+
+        const Box = artifacts.require('Box')
+        const box = await Box.deployed();
+
+        const value = await box.retrieve();
+        console.log('Box value is', value.toString())
+
+        callback(0);
+    } catch (error) {
+        console.error(error);
+        callback(1);
+    }
+};
